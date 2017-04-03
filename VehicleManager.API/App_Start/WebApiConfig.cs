864591d5c2d75b8142f79bdeb31503c313f8bc66
Arlin Grijalba <Arlin Grijalba>
2017-04-03 16:01:51 -0700

    App_Start file ready

--- VehicleManager.API/App_Start/WebApiConfig.cs
+++ VehicleManager.API/App_Start/WebApiConfig.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json.Serialization;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace VehicleManager.API
 {
@@ -19,6 +18,15 @@ namespace VehicleManager.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // enable cors
+            config.EnableCors(new EnableCorsAttribute("", "", ""));
+
+            // camel case resolve
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // xml formatting
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
